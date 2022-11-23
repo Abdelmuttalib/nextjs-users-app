a7685167a381df12ed985b6204c43cfefa8421f3
Abdelmuttalib <hagar9886@gmail.com>
2022-11-23 17:40:55 +0800

    feat(users): add useUsers hook to seperate logic

--- app/Users.tsx
+++ app/Users.tsx
@@ -1,68 +1,11 @@
 'use client';
-import { ResponseT } from 'app/types';
+import useUsers from 'app/use-users';
 import User, { UserLoader } from 'app/User';
-import { useState } from 'react';
-import useSWR from 'swr';
 
 import clsxm from '@/lib/clsxm';
 
-const usersApiUrl = 'https://reqres.in/api/users';
-
-const fetcher = (apiUrl: string) => fetch(apiUrl).then((res) => res.json());
-
 const Users = () => {
-  const [showingAllUsers, setShowingAllUsers] = useState<boolean>(false);
-  const [page] = useState<number>(1);
-  const { data, mutate, error } = useSWR<ResponseT>(usersApiUrl, fetcher, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    revalidateIfStale: false,
-  });
-
-  const isLoading = !data && !error;
-
-  const fetchMoreUsers = async (page: number) => {
-    try {
-      const response = await fetch(`${usersApiUrl}?page=${page}`);
-      const data: ResponseT = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error(error as string);
-    }
-  };
-
-  const mutateCacheData = (newData: ResponseT) => {
-    mutate(newData, {
-      revalidate: false,
-    });
-  };
-
-  const getNextPage = () => {
-    if (data) {
-      const { total_pages: totalPages } = data;
-
-      if (totalPages && page < totalPages) {
-        const nextPage = page + 1;
-        return nextPage;
-      }
-    }
-
-    return page;
-  };
-
-  const loadMoreUsers = async () => {
-    const nextPage = getNextPage();
-    const usersData = await fetchMoreUsers(nextPage);
-
-    if (data && usersData) {
-      mutateCacheData({
-        ...data,
-        data: [...data.data, ...usersData.data],
-      });
-    }
-
-    setShowingAllUsers(true);
-  };
+  const { data, isLoading, loadMoreUsers, showingAllUsers } = useUsers();
 
   return (
     <div className='flex flex-col gap-3 py-10'>
