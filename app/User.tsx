3db77cf96ab66ebc42d7d2edc88a38d5a48dffe2
Abdelmuttalib <hagar9886@gmail.com>
2022-12-11 22:09:06 +0800

    feat(users-grid): fix users cards grid template columns

--- app/User.tsx
+++ app/User.tsx
@@ -11,7 +11,12 @@ const User: FC<UserT> = ({
   email,
 }: UserT): ReactElement => {
   return (
-    <div className='my-2 flex w-64 items-center gap-2 rounded bg-white py-1.5 px-3 shadow-lg ring-2 ring-gray-700 ring-offset-2 md:w-80 md:py-2.5 md:px-5'>
+    <div
+      className='relative my-2 grid auto-cols-max grid-flow-col items-center gap-2 rounded-lg border-2 border-gray-700 bg-white px-3 py-1.5 shadow-lg md:py-2.5 md:px-5'
+      style={{
+        gridTemplateColumns: '100px max-content',
+      }}
+    >
       <Image
         src={avatar}
         alt={`${first_name} avatar`}
@@ -19,11 +24,11 @@ const User: FC<UserT> = ({
         height={80}
         className='rounded-full'
       />
-      <div>
+      <div className='w-full'>
         <p className='text-sm font-medium md:text-base'>
           {first_name} {last_name}
         </p>
-        <span className='w-full truncate text-xs text-gray-500 md:text-sm'>
+        <span className='inline-block w-full overflow-hidden text-ellipsis text-xs text-gray-500 md:text-sm'>
           {email}
         </span>
       </div>
