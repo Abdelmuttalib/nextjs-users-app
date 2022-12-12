f32341d025521df8e3ac3ff244212457018838e4
Abdelmuttalib <hagar9886@gmail.com>
2022-12-12 14:53:21 +0800

    feat(users): add text truncate for smaller screen sizes for user name and email

--- app/User.tsx
+++ app/User.tsx
@@ -11,7 +11,7 @@ const User: FC<UserT> = ({
   email,
 }: UserT): ReactElement => {
   return (
-    <div className='relative my-2 grid auto-cols-max grid-flow-col items-center gap-3 rounded-lg border-2 border-gray-700 bg-white px-3 py-1.5 shadow-lg md:py-2.5 md:px-5'>
+    <div className='relative my-2 grid w-full grid-flow-col place-items-start items-center justify-start justify-items-start gap-2 rounded-lg border-2 border-gray-700 bg-white px-3 py-1.5 pr-4 shadow-lg sm:gap-4 md:py-2.5 md:px-5'>
       <Image
         src={avatar}
         alt={`${first_name} avatar`}
@@ -19,11 +19,11 @@ const User: FC<UserT> = ({
         height={80}
         className='rounded-full'
       />
-      <div>
-        <p className='text-sm font-medium md:text-base'>
+      <div className='max-w-full truncate'>
+        <p className='truncate text-sm font-medium md:text-base'>
           {first_name} {last_name}
         </p>
-        <span className='inline-block w-full overflow-hidden text-ellipsis text-xs text-gray-500 md:text-sm'>
+        <span className='inline-block w-full truncate text-xs text-gray-500 md:text-sm'>
           {email}
         </span>
       </div>
@@ -34,9 +34,9 @@ const User: FC<UserT> = ({
 export default User;
 
 export const UserLoader: FC = (): ReactElement => (
-  <div className='relative my-2 grid auto-cols-max grid-flow-col items-center gap-3 rounded-lg border-2 border-gray-100 bg-white px-3 py-1.5 shadow-lg md:py-2.5 md:px-5'>
+  <div className='relative my-2 grid w-full grid-flow-col place-items-start items-center justify-start justify-items-start gap-2 rounded-lg border-2 border-gray-100 bg-white px-3 py-1.5 shadow-lg sm:gap-4 md:py-2.5 md:px-5'>
     <SkeletonLoader className='h-20 w-20 rounded-full' />
-    <div className='flex flex-col gap-1'>
+    <div className='flex w-full flex-col gap-1 overflow-hidden'>
       <div className='font-medium'>
         <SkeletonLoader className='h-5 w-24' />
       </div>
